eaaf553d710d4ee2abc9a2d7ee26a738f3f8d927
Naresh Mengani <nareshmengani069@gmail.com>
2025-08-06 23:59:46 +0530

    Add files via upload

---
+++ Arrays/Arrayselectionsort.java
@@ -0,0 +1,28 @@
+//selection sort
+import java.util.Scanner;
+class Arrayselectionsort{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int arr[]=new int[5];
+        System.out.println("Enter the elements into the array");
+        for(int i=0;i<5;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length-1;i++){
+            int minindex=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[minindex]){
+                    minindex=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[minindex];
+            arr[minindex]=temp;
+            
+        }
+        System.out.println("the sorted array is");
+        for(int  num : arr){
+            System.out.print(num+" ");
+        }
+    }
+}
